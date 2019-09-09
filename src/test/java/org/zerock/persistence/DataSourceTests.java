42b05537a901198292f89975c76927e6beba3126
semanticker <semanticker@gmail.com>
2019-09-09 17:33:55 +0900

    히카리 테스트

--- src/test/java/org/zerock/persistence/DataSourceTests.java
+++ src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -1,6 +1,10 @@
-package org.zerock.sample;
+package org.zerock.persistence;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,19 +18,17 @@ import lombok.extern.log4j.Log4j;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
-public class SampleTests {
-
+public class DataSourceTests {
 	@Setter(onMethod_ = {@Autowired})
-	private Restaurant restaurant;
+	private DataSource dataSource;
 	
 	@Test
-	public void testExist() {
-		assertNotNull(restaurant);
-		
-		log.info(restaurant);
-		log.info("------------------------------------");
-		log.info(restaurant.getChef());
-		
+	public void testConnection() {
+		try(Connection con = dataSource.getConnection()){
+			log.info(con);
+		}catch(Exception e){
+			fail(e.getMessage());
+		}
 	}
 	
 }
